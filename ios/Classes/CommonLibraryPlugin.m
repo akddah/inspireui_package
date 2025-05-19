2d57b4cd351ca7c031b5d02826e7701552ca2118
Abdallah <ao25332@gmail.com>
2025-05-19 22:17:21 +0300

    fix ios

--- ios/Classes/CommonLibraryPlugin.m
+++ ios/Classes/CommonLibraryPlugin.m
@@ -1,11 +1,11 @@
 #import "CommonLibraryPlugin.h"
-#if __has_include(<inspireui/inspireui-Swift.h>)
-#import <inspireui/inspireui-Swift.h>
+#if __has_include(<inspireui_package/inspireui_package-Swift.h>)
+#import <inspireui_package/inspireui_package-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "inspireui-Swift.h"
+#import "inspireui_package-Swift.h"
 #endif
 
 @implementation CommonLibraryPlugin
